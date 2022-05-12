b106e32017e86068d66d23f8b15b8771d596498a
chxn_lin@163.com <chxn_lin@163.com>
2022-05-12 14:43:29 +0800

    first commit

---
+++ mall-producet/src/test/java/com/chxn/mall/producet/MallProducetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chxn.mall.producet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallProducetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
